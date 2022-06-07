0d46a7de538184b3e8006e086e37c0f3ec9e7a58
tung3110 <nguyenhuytung@gmail.com>
2022-06-07 14:55:22 +0700

    update 1

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ def hello2():
     return "Xin Chao API by Docker Container jenkins OK "
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 3000))
     app.run(debug=True,host='0.0.0.0',port=port)
